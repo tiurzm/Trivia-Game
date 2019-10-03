0b7c69aa797a78686bfe20ddeffb92ad46290c9b
Tiur Mawaddah <tiurzm@gmail.com>
2019-10-03 16:19:22 -0500

    <COMMENT>

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,47 +4,50 @@ $(document).ready(function(){
         {
             question: "1.Which movie includes a clear homage to Marlon Brando's character in The Godfather?",
             answers: {a: "Zootopia", b: "Cinderella", c:"Tarzan", d: "Beauty and the Beast"},
-            correctAnswer: "a",
+            correctAnswer: "Zootopia",
         }, {
             question: "2.Which movie references Elvi's unmistakable white jumpsuit?",
             answers: {a: "The Hunback of Notre Dame", b: "Lilo & Stitch", c: "Oliver & Company", d: "A Bug's Life"},
-            correctAnswer: "b",
+            correctAnswer: "Lilo & Stitch",
         }, {
             question: "3.Which movie features a character transforming into Jack Nicholson?",
             answers: {a: "The Little Mermaid", b: "Hercules", c: "Alladin", d: "Brother Bear"},
-            correctAnswer: "c",
+            correctAnswer: "Alladin",
         }, {
             question: "4.Which movie references Robert De Niro's famous speech in taxi dirver?",
             answers: {a: "Beauty and the Beast", b: "The lion King", c: "The Great Mouse Detective", d: "Tangled"},
-            correctAnswer: "b",
+            correctAnswer: "The lion King",
         }, {
             question: "5.Which movie includes Browser from Super Mario Bros?",
             answers: {a: "Big Hero 6", b: "Toy Story", c: "Wall-E", d: "Wreck-It Ralph"},
-            correctAnswer: "d",
+            correctAnswer: "Wreck-It Ralph",
         }, {
             question: "6.Which Disney-Pixar movie features part of the iconic score from Psycho?",
             answers: {a: "Finding Nemo", b: "Monster,Inc", c: "A Bug's Life", d: "Finding Dory"},
-            correctAnswer: "a",
+            correctAnswer: "Finding Nemo",
         },{
             question: "7.Which movie uses music from The Haunted Mansion Ride in a nightmare scene?",
             answers: {a: "Frozen", b: "Winnie the Pooh", c: "Inside Out", d: "Atlantis: The Lost Empire"},
-            correctAnswer: "c",
+            correctAnswer: "Inside Out",
         },{
             question: "8.Which movie references Independence Day with line 'I have got to get me one of these'?",
             answers: {a: "Tangled", b: "The Princess and the Frog", c: "Frozen", d: "Big Hero 6"},
-            correctAnswer: "a",
+            correctAnswer: "Tangled",
         },{
             question: "9.Which movie features a character's head spinning all the way around like Regan's in The Exorcist?",
             answers: {a: "The Emperor's New Groove", b: "Toy Story", c: "The Black Cauldron", d: "Lilo & Stich"},
-            correctAnswer: "b",
+            correctAnswer: "Toy Story",
         },{
             question: "10.Which movie features a villain saying a version of the line 'Lucy, I'm home' from I Love Lucy?",
             answers: {a: "The Emperor's New Groove", b: "The Rescuers", c: "The Hunchback of Notre Dame", d: "Hercules"},
-            correctAnswer: "d",
+            correctAnswer: "Hercules",
         },
     ];
     console.log(allQuestion);
-    console.log(allQuestion[0].correctAnswer);
+    for( var i = 0; i < allQuestion.length; i++){
+        console.log(allQuestion[i].correctAnswer);
+    }
+    console.log(allQuestion[0]);
 
     var timeRemain = 60;
     var intervalId;
@@ -62,35 +65,32 @@ $(document).ready(function(){
         question1.addClass("question");
         $(".timer").append(question1);
         question();
-        radio();
+        answer();
         // display done button
         $(".timer").append("<button class=submit>Done</button>");
-        run();
+        // run the time
+        // run();
         $(".submit").on("click", function(){
             stop();
             done();
-            if(right === allQuestion[0].correctAnswer){
-                console.log("good");
-            }
         });
         
         // display all question:
         function question(){
             for(var i = 0; i < allQuestion.length; i++){
             $(".question").append("<p>"+ allQuestion[i].question +"</p>");
-            $(".question").append("<input class=for type=radio><label class=check>"+allQuestion[i].answers.a+"</label> <input class=for type=radio><label class=check>"+allQuestion[i].answers.b+"</label> <input class=for type=radio><label class=check>"+allQuestion[i].answers.c+"</label> <input class=for type=radio><label class=check>"+allQuestion[i].answers.d+"</label>");
+            $(".question").append("<input class=check type=radio><label class=for>"+allQuestion[i].answers.a+"</label> <input class=check type=radio><label class=for>"+allQuestion[i].answers.b+"</label> <input class=check type=radio><label class=for>"+allQuestion[i].answers.c+"</label> <input class=check type=radio><label class=for>"+allQuestion[i].answers.d+"</label>");
             }
         };
 
-        function radio(){
-            $("input:radio").each(function() {
-                if($(this).is(":checked")){
-                    console.log("check"); 
-                }
-            });
-        }
-
-        
+        // set data answer 
+        function answer(){
+            $(".for").each(function() {
+                $(this).attr("data-answer", allQuestion[i].correctAnswer);
+            }); 
+        };
+    
+    
         // set timer
         function run(){
             clearInterval(intervalId);
